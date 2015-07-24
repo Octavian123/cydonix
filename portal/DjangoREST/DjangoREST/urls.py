f3bb04273b444f99024aa8a5892c13da3486fd0d
Octavian123 <rusu_tavi2007@yahoo.com>
2015-07-24 15:51:18 +0300

    Update urls.py

--- portal/DjangoREST/DjangoREST/urls.py
+++ portal/DjangoREST/DjangoREST/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^portal/$', 'portal.views.sensor_data_list'),
     url(r'^portal/temperature$', 'portal.views.temperature_list'),
     url(r'^portal/pressure$', 'portal.views.pressure_list'),
